d8d41dfec5c899386a349d4d7eb2b6bf24e21c9a
Vijay Singh <vijaysinghaashu@gmail.com>
2023-11-27 10:21:44 +0530

    springboot-jpa-auditing

---
+++ src/main/java/com/excellence/springbootlogginglogback/Springboot3JpaAuditingApplication.java
@@ -0,0 +1,13 @@
+package com.excellence.springbootlogginglogback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3JpaAuditingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3JpaAuditingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/excellence/springbootlogginglogback/Springboot3JpaAuditingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excellence.springbootlogginglogback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3JpaAuditingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
